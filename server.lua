a3cd3cefba6279c6795b9c5ef3628ddfa7dfcf58
Kanersps <kanersps@pm.me>
2019-01-12 20:56:19 +0100

    Forgot renaming

--- server.lua
+++ server.lua
@@ -269,7 +269,7 @@ RegisterCommand('setgroup', function(source, args, raw)
 
 				if(groups[args[2]])then
 					TriggerEvent("es:getPlayerFromId", player, function(user)
-						ExecuteCommand('remove_principal identifier.' .. player.identifier .. " group." .. player.group)
+						ExecuteCommand('remove_principal identifier.' .. user.getIdentifier() .. " group." .. user.getGroup())
 
 						TriggerEvent("es:setPlayerData", player, "group", args[2], function(response, success)
 							TriggerClientEvent('es:setPlayerDecorator', player, 'group', tonumber(group), true)
@@ -277,7 +277,7 @@ RegisterCommand('setgroup', function(source, args, raw)
 								args = {"^1CONSOLE", "Group of ^2^*" .. GetPlayerName(player) .. "^r^0 has been set to ^2^*" .. group}
 							})
 
-							ExecuteCommand('add_principal identifier.' .. player.identifier .. " group." .. player.group)
+							ExecuteCommand('add_principal identifier.' .. user.getIdentifier() .. " group." .. user.getGroup())
 						end)
 					end)
 				else
